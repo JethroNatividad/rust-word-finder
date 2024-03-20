ce7000a81ef169284aa8c91601a7d694fca62dcb
JethroNatividad <natividad.jet@gmail.com>
2024-03-21 06:25:21 +0800

    initialize tests, aadd return values

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@
 // process: find the word utilize, and replace it with use
 // outputs: write to the file.
 
-fn read_file(filename: String) -> String {}
-fn replace_occurence(text: String, find: String, replace: String) -> String {}
+fn read_file(filename: String) -> String {
+    return "".to_string();
+}
+fn replace_occurence(text: String, find: String, replace: String) -> String {
+    return "".to_string();
+}
 fn write_file(text: String, filename: String) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_file() {}
+}
 
 fn main() {
     println!("Hello World!")
