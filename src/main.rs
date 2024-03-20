4dcebf5238e8a0a2f75f30322b106f378092a386
JethroNatividad <natividad.jet@gmail.com>
2024-03-21 06:15:52 +0800

    Expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,4 @@
 // Write a program that reads an input text file, looks for an occurence of a word, replace that word with another else, and write to that file.
+// Inputs: file
+// process: find the word utilize, and replace it with use
+// outputs: write to the file.
